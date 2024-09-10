5b2c0fd3b5832dae92d5c0ef0e833f61c7c4a884
Sushil Kumar Verma <114611314+sushilverma1125@users.noreply.github.com>
2024-09-10 11:35:12 +0530

    bs using recursion

--- Binary Search/binarySearch.cpp
+++ Binary Search/binarySearch.cpp
@@ -1,3 +1,4 @@
+// Iterative approach
 int search(vector<int> &nums, int target) {
     // Write your code here.
     int low = 0, high = nums.size() - 1;
@@ -9,4 +10,17 @@ int search(vector<int> &nums, int target) {
       else high = mid - 1;
     }
     return -1;
+}
+
+// Recursive approach
+int recBinarySearch(vector<int> &nums, int low, int high, int target) {
+    if(low > high)  return -1;
+    int mid = (low + high) / 2;
+    if(nums[mid] == target) return mid;
+    else if(target > nums[mid]) return recBinarySearch(nums, mid + 1, high, target);
+    return recBinarySearch(nums, low, high - 1, target);
+}
+
+int search(vector<int> &nums, int target) {
+    return recBinarySearch(nums, 0, nums.size() - 1, target);
 }
