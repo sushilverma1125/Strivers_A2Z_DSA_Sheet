599837c4274aa00c86bcfe382d3c79171f6f9baa
Sushil Kumar Verma <114611314+sushilverma1125@users.noreply.github.com>
2024-09-10 11:13:54 +0530

    binary search

---
+++ Binary Search/binarySearch.cpp
@@ -0,0 +1,12 @@
+int search(vector<int> &nums, int target) {
+    // Write your code here.
+    int low = 0, high = nums.size() - 1;
+
+    while(low <= high) {
+      int mid = (low + high) / 2;
+      if(nums[mid] == target) return mid;
+      else if (target > nums[mid]) low = mid + 1;
+      else high = mid - 1;
+    }
+    return -1;
+}
