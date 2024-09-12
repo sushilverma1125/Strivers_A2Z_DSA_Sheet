c900b36e49995508ca3f355454d5c6c2b533e332
Sushil Kumar Verma <114611314+sushilverma1125@users.noreply.github.com>
2024-09-12 21:19:25 +0530

    lower bound & upper bound

--- Binary Search/lowerBound.cpp
+++ Binary Search/lowerBound.cpp
@@ -19,3 +19,21 @@ int lowerBound(vector<int> arr, int n, int x) {
 }
 
 // upper bound -> smallest index such that arr[index] > x
+
+int lowerBound(vector<int> arr, int n, int x) {
+	// Write your code here
+	int low = 0, high = n - 1;
+	int ans = n;
+	while(low <= high) {
+		int mid = (low + high) / 2;
+		// maybe an answer
+		if(arr[mid] > x) {  // change was here only from >= to >
+			ans = mid;
+			// look for more small index on left
+			high = mid - 1;
+		} else {
+			low = mid + 1; // look for right 
+		}
+	}
+	return ans;
+}
