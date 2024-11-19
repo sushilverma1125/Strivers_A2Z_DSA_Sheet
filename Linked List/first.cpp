b8ceff629aafda2f9b4edce10a6752bf37f3f365
Sushil Kumar Verma <114611314+sushilverma1125@users.noreply.github.com>
2024-11-19 10:47:52 +0530

    Linked list

--- Linked List/first.cpp
+++ Linked List/first.cpp
@@ -14,7 +14,33 @@ class Node
     }
 };
 
-int main () {
+Node *convertToLinkedList(vector<int> &arr) {
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    
+    for(int j = 1; j < arr.size();j++) {
+        Node *temp = new Node(arr[j]);
+        mover -> next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int main()
+{
     Node *x = new Node(2); 
-    cout << x->data; // OUTPUT: 2
+    cout << x->data << endl; // OUTPUT: 2
+
+    vector<int> arr = {12, 3, 4, 5, 6, 7};
+    Node *y = convertToLinkedList(arr);
+    cout << y -> data << endl; // OUTPUT: 12
+    
+    // To traverse the linked list
+    Node *temp = y;
+    while(temp != NULL) {
+        cout << temp -> data << "->";
+        temp = temp -> next;
+    }
+
+    return 0;
 }
