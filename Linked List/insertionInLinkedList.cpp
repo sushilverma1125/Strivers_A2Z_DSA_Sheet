d2a0e9f9189728dcbf9be4933b1fd77bb559f2c4
Sushil Kumar Verma <114611314+sushilverma1125@users.noreply.github.com>
2024-11-28 14:36:58 +0530

    insertion in linked list

--- Linked List/insertionInLinkedList.cpp
+++ Linked List/insertionInLinkedList.cpp
@@ -51,6 +51,57 @@ Node *insertAtHead(Node *head, int ele) {
     return temp;
 }
 
+Node *insertAtTail(Node *head, int ele) {
+    if(head == NULL) {
+        return new Node(ele);
+    } 
+
+    Node *temp = head;
+    while(temp -> next != NULL) {
+        temp = temp -> next;
+    }
+
+    Node *newNode = new Node(ele);
+    temp -> next = newNode;
+    return head;
+}
+
+Node* insertAtPoisiton(Node *head, int ele, int k) {
+    // Case when the head is NULL, meaning the list is empty
+    if (head == nullptr) {
+        if (k == 1) {
+            return new Node(ele);
+        } else {
+            return nullptr;  // Invalid position
+        }
+    }
+
+    // If k is 1, we can directly insert at the head
+    if (k == 1) {
+        return new Node(ele, head);
+    }
+
+    int cnt = 1;  // Start counting from the first node
+    Node *temp = head;
+    
+    // Traverse until we reach the (k-1)th node (position before the desired insertion point)
+    while (temp != nullptr && cnt < k - 1) {
+        cnt++;
+        temp = temp->next;
+    }
+
+    // If we've reached a valid node, insert the new element
+    if (temp != nullptr) {
+        Node *newNode = new Node(ele, temp->next);
+        temp->next = newNode;
+    } else {
+        // If temp is NULL, k was greater than the length of the list, so we do nothing
+        cout << "Position out of bounds" << endl;
+    }
+
+    return head;
+}
+
 // Main function for testing createLinkedList functionality
 int main() {
     vector<int> arr = {12, 3, 4, 5, 6, 7};
@@ -63,6 +114,16 @@ int main() {
     // After inserting the element at head
     head = insertAtHead(head, 11);
     cout << "After inserting the element at head: ";
+    print(head);
+
+    // After inserting the element at tail
+    head = insertAtTail(head, 8);
+    cout << "After inserting the element at tail: ";
+    print(head);
+
+    // After inserting the element at any position
+    head = insertAtPoisiton(head, 9, 9);
+    cout << "After inserting the element at given position: ";
     print(head);
 
     return 0;
